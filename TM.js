73565f1dc7447a984712adc091bb5d797f8e521c
imaverage <73820097+imaverage@users.noreply.github.com>
2024-07-19 14:29:14 -0500

    a

--- TM.js
+++ TM.js
@@ -9,6 +9,28 @@
   const getStopButton = () => Mine.qsaa('button').find(e => e.innerText === 'Stop');
   const isAiTyping = () => !!getStopButton();
   const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  const getIsChatScrolledToNearBottom = (threshold = 0) => {
+    const element = Mine.qs('html');
+    return element.scrollHeight - element.scrollTop <= element.clientHeight + threshold +1;
+  };
+
+  // scrolling
+  const originalScrollTo = window.scrollTo;
+  let allowJsScrolling = true;
+  window.scrollTo = (...args) => {
+    if (!allowJsScrolling) return;
+
+    const firstArg = args[0];
+    const options =
+      (typeof firstArg === 'object')
+        ? firstArg
+        : { top: firstArg, left: args[1] };
+    options.behavior = 'smooth';
+
+    originalScrollTo.call(window, options);
+  };
+  const turnOffJsScrolling = () => allowJsScrolling = false;
+  const turnOnJsScrolling = () => allowJsScrolling = true;
 
   Mine.quietQs(`[data-element-id="pinned-characters-container"]`);
   Mine.isi(`
@@ -1698,15 +1720,23 @@
     await main();
   }
 
-  const installPinnedMsgScroller = async () => {
-    const bindOnSelectorClick = async (qs, cb) => {
-      // TODO: just have a global handler rather than a new one for each?
-      document.body.addEventListener('touchend', async e => {
-        const targetEle = e.target.closest(qs);
-        if (!targetEle) return;
-        await cb(targetEle);
-      });
+  const bindOnSelectorClick = async (qs, cb) => {
+    // TODO: just have a global handler rather than a new one for each?
+    const clickProxyEventName = isMobile ? 'touchend' : 'click';
+    document.body.addEventListener(clickProxyEventName, async e => {
+      const targetEle = e.target.closest(qs);
+      if (!targetEle) return;
+      await cb(targetEle);
+    });
+  }
+  bindOnSelectorClick(`[data-element-id="send-button"]`, () => {
+    // allows for slow quote replied type convos
+    if (!getIsChatScrolledToNearBottom(300)) {
+      turnOffJsScrolling();
+      setTimeout(turnOnJsScrolling, 3000);
     }
+  });
+  const installPinnedMsgScroller = async () => {
     bindOnSelectorClick('button:has([d="M878.3 392.1L631.9 145.7c-6.5-6.5-15-9.7-23.5-9.7s-17 3.2-23.5 9.7L423.8 306.9c-12.2-1.4-24.5-2-36.8-2-73.2 0-146.4 24.1-206.5 72.3a33.23 33.23 0 0 0-2.7 49.4l181.7 181.7-215.4 215.2a15.8 15.8 0 0 0-4.6 9.8l-3.4 37.2c-.9 9.4 6.6 17.4 15.9 17.4.5 0 1 0 1.5-.1l37.2-3.4c3.7-.3 7.2-2 9.8-4.6l215.4-215.4 181.7 181.7c6.5 6.5 15 9.7 23.5 9.7 9.7 0 19.3-4.2 25.9-12.4 56.3-70.3 79.7-158.3 70.2-243.4l161.1-161.1c12.9-12.8 12.9-33.8 0-46.8zM666.2 549.3l-24.5 24.5 3.8 34.4a259.92 259.92 0 0 1-30.4 153.9L262 408.8c12.9-7.1 26.3-13.1 40.3-17.9 27.2-9.4 55.7-14.1 84.7-14.1 9.6 0 19.3.5 28.9 1.6l34.4 3.8 24.5-24.5L608.5 224 800 415.5 666.2 549.3z"])', async e => {
       const dialogPinnedMsgs = await Mine.waitFor(() => {
         const eles = Mine.qsaa(`[data-element-id="pop-up-modal"] [data-element-id="response-block"]`);
