9c274e50c84047bf99ac36406a10f3f67614f1b3
imaverage <73820097+imaverage@users.noreply.github.com>
2024-07-28 17:50:12 -0500

    a

--- TM.js
+++ TM.js
@@ -280,7 +280,7 @@ body {
 [data-element-id="list-more-button"] {
   display: none;
 }
-`);  // TODO: some of this like the hotpink stuff is pasted twice
+`);
 
   const attachMetaInfoV1 = async () => {
     const infoIconPath = `M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z`;
