876eb1acf5d89deba30756f856ccdb05b051a360
imaverage <73820097+imaverage@users.noreply.github.com>
2024-08-08 01:44:12 -0500

    a

--- TM.js
+++ TM.js
@@ -1357,7 +1357,7 @@ button[data-element-id="output-settings-button"] {
         const updatePlaceholder = () => {
           taEle.placeholder = 'Message';
         }
-        ['blur', 'focus'].forEach(eventName => taEle.addEventListener(eventName, () => requestAnimationFrame(updatePlaceholder)));
+        ['blur', 'focus', 'input'].forEach(eventName => taEle.addEventListener(eventName, () => requestAnimationFrame(updatePlaceholder)));
         await getAnimFrameAsync();
         setTimeout(updatePlaceholder, 0);
       });
